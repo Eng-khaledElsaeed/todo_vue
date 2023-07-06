31999eca309fede3db89a5ddf0ef3cef55bfe7c4
khaled elsaeed abdelfattah <khaledelsaied62@gmail.com>
2023-07-07 02:14:01 +0300

    some files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>My Todo List</h1>
+<div class="grid">
+	<todo_list></todo_list>
+	<todo_history></todo_history>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todo_list from "./components/todo.vue";
+import todo_history from "./components/todo_history.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+components: {
+	todo_list,
+	todo_history
+}
 }
 </script>
+  
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style lang="scss">
+$size1: 6px;
+$size2: 12px;
+$size3: 18px;
+$size4: 24px;
+$size5: 48px;
+
+$backgroundColor: #27292d;
+$textColor: white;
+$primaryColor: #a0a4d9;
+$secondTextColor: #1f2023;
+body {
+	margin: 0;
+	padding: 0;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: $backgroundColor;
+	color: $textColor;
+	h1{
+		font-weight: bold;
+		text-align: center;
+	}
+	.grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+	margin-left: auto;
+	margin-right: auto;
+	padding: 20px;
+	max-width: 90%;
+    gap:.5rem 2rem;
+}
 }
 </style>
---
+++ src/components/todo_history.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="todo_hist">
+        <h1>todo history</h1>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'todo_history',
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
